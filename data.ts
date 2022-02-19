d23bf95029623976fb5ad0e79416df939354bd7f
Betari Mehdi <mehdi.betari@gmail.com>
2022-02-19 17:56:00 +0100

    refactor: export data from index

---
+++ data.ts
@@ -0,0 +1,29 @@
+import { Drug } from "./src/types";
+
+export const drugs: Array<Drug> = [
+	{
+		name: "Doliprane",
+		expiresIn: 20,
+		benefit: 30,
+	},
+	{
+		name: "Herbal Tea",
+		expiresIn: 10,
+		benefit: 5,
+	},
+	{
+		name: "Fervex",
+		expiresIn: 5,
+		benefit: 40,
+	},
+	{
+		name: "Magic Pill",
+		expiresIn: 15,
+		benefit: 40,
+	},
+	{
+		name: "Dafalgan",
+		expiresIn: 15,
+		benefit: 40,
+	},
+];
--- index.ts
+++ index.ts
@@ -1,34 +1,6 @@
 import { writeFile } from "fs";
-import { DrugProps } from './types';
 import { updateBenefitValue } from "./pharmacy";
-
-const drugs: Array<DrugProps> = [
-  {
-    name: "Doliprane",
-    expiresIn: 20,
-    benefit: 30,
-  },
-  {
-    name: "Herbal Tea",
-    expiresIn: 10,
-    benefit: 5,
-  },
-  {
-    name: "Fervex",
-    expiresIn: 5,
-    benefit: 40,
-  },
-  {
-    name: "Magic Pill",
-    expiresIn: 15,
-    benefit: 40,
-  },
-  {
-    name: "Dafalgan",
-    expiresIn: 15,
-    benefit: 40,
-  },
-];
+import { drugs } from "./data";
 
 const logs: Array<string> = [];
 
